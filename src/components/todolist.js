fff93332f941a8059cfb246235c4a9910994ac90
Urooj Ahmad <uroojahamad786@gmail.com>
2022-05-16 23:58:01 +0530

    renaming class attribute to className

--- src/components/todolist.js
+++ src/components/todolist.js
@@ -3,20 +3,20 @@ import React from 'react';
 
 const TodoList = () => {
     return (
-        <div class="container d-flex-column">
-            <h1 class="text-center">Todo List</h1>
-            <div class="subcontainer input-group">
-                <input type="text" class="form-control" id="todoItem" placeholder="Add a todo item" />
-                <button class="btn" id="addItemToList"><i class="fa fa-plus"></i>Add</button>
+        <div className="container d-flex-column">
+            <h1 className="text-center">Todo List</h1>
+            <div className="subcontainer input-group">
+                <input type="text" className="form-control" id="todoItem" placeholder="Add a todo item" />
+                <button className="btn" id="addItemToList"><i className="fa fa-plus"></i>Add</button>
             </div>
-            <div class="subcontainer d-flex-row">
-                <div class="task-list">
+            <div className="subcontainer d-flex-row">
+                <div className="task-list">
                     <h4>Your Items</h4>
-                    <ul class="list-group list-group-flush" id="listItem"></ul>
+                    <ul className="list-group list-group-flush" id="listItem"></ul>
                 </div>
-                <div class="completed-list">
+                <div className="completed-list">
                     <h4>Completed Items</h4>
-                    <ul class="list-group list-group-flush" id="completedListItem"></ul>
+                    <ul className="list-group list-group-flush" id="completedListItem"></ul>
                 </div>
             </div>
         </div>
