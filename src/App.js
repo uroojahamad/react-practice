e120d26f8adea1474a10867c72ced1b872a47670
Urooj Ahmad <uroojahamad786@gmail.com>
2022-05-17 12:22:52 +0530

    adding Todo List

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 // import './App.css';
 import Counter from './components/counter';
 import DiceRoll from './components/DiceRoll';
+import TodoList from './components/todolist';
 
 function App() {
   return (
     <div className="App">
-      <Counter/>
+      {/* <Counter/> */}
       {/* <DiceRoll/> */}
+      <TodoList/>
     </div>
   );
 }
