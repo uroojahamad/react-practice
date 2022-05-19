bcd1888767376eb58b2dce0d6d02cb13fd6d96c2
Urooj Ahmad <uroojahamad786@gmail.com>
2022-05-19 22:55:15 +0530

    addded fuunction to get the value of textbox

--- src/components/todolist.js
+++ src/components/todolist.js
@@ -2,19 +2,20 @@ import React, { useState } from 'react';
 import '../css/todolist.css';
 
 const TodoList = () => {
+    const [tasks, setTasks] = useState('');
     
-
+   
     return (
         <div className="todolist-container d-flex-column">
             <h1 className="text-center">Todo List</h1>
             <div className="todolist-subcontainer input-group">
-                <input type="text" className="form-control" id="todoItem" placeholder="Add a todo item" />
-                <button className="todolist-btn" id="addItemToList"><i className="fa fa-plus"></i>Add</button>
+                <input type="text" className="form-control" id="todoItem" placeholder="Add a todo item" onChange={(e) => setTask(e.target.value)}/>
+                <button className="todolist-btn" id="addItemToList" onClick={addTask}><i className="fa fa-plus"></i>Add</button>
             </div>
             <div className="todolist-subcontainer d-flex-row">
                 <div className="task-list">
                     <h2>Your Items</h2>
-                    <ul className="list-group list-group-flush" id="listItem"></ul>
+                    <ul className="list-group list-group-flush" id="listItem">{task}</ul>
                 </div>
                 <div className="completed-list">
                     <h2>Completed Items</h2>
