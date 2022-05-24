8acf6dee2bda216fbc00a7cd2cd19871f528768a
Urooj Ahmad <uroojahamad786@gmail.com>
2022-05-24 23:34:06 +0530

    removed unused import

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Counter from './components/counter';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
