dc4103fe51c9485b792bdbbd6c6b90753ecb53c7
Urooj Ahmad <uroojahamad786@gmail.com>
2023-01-27 22:02:36 +0530

    adding css styles

--- src/components/ScrimbaProject1.js
+++ src/components/ScrimbaProject1.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import '../css/scrimbaproject1.css'
 
 const ScrimbaProject1 = () => {
     return (
         <div>
-            <img src={require(`../images/logo192.png`)} alt="react logo" />
-            <h1>Fun Facts about React</h1>
-            <ul>
-                <li>Was first released in 2013</li>
-                <li>Was originally created by Jordan Walke</li>
-                <li>Has well over 100k stars on GitHub</li>
-                <li>Is maintained by Facebook</li>
-                <li>Powers thousands of enterprise apps, including mobile apps</li>
-            </ul>
+            <nav>
+                <img src={require(`../images/logo192.png`)} alt="react logo" />
+                <h3>ReactFacts</h3>
+                <h4>React Course - Project 1</h4>
+            </nav>
+            <main>
+                <h1 className='main--title'>Fun Facts about React</h1>
+                <ul className='main--facts'>
+                    <li>Was first released in 2013</li>
+                    <li>Was originally created by Jordan Walke</li>
+                    <li>Has well over 100k stars on GitHub</li>
+                    <li>Is maintained by Facebook</li>
+                    <li>Powers thousands of enterprise apps, including mobile apps</li>
+                </ul>
+            </main>
         </div>
     )
 }
