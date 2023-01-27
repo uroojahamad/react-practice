4be98d3d106b6a0a18b8486e473887095fbca49b
Urooj Ahmad <uroojahamad786@gmail.com>
2023-01-27 16:32:47 +0530

    added scrimba project 1 component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import logo from './logo.svg';
 import Counter from './components/counter';
 import DiceRoll from './components/DiceRoll';
 import TodoList from './components/todolist';
+import ScrimbaProject1 from './components/ScrimbaProject1'
 
 function App() {
   return (
     <div className="App">
       {/* <Counter/> */}
       {/* <DiceRoll/> */}
-      <TodoList/>
+      {/* <TodoList/> */}
+      <ScrimbaProject1/>
     </div>
   );
 }
