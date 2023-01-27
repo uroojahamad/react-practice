41ed6cff7e65934c7e730d9a952f185b06336e32
Urooj Ahmad <uroojahamad786@gmail.com>
2023-01-27 16:33:32 +0530

    added scrimba project1.js file

---
+++ src/components/ScrimbaProject1.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const ScrimbaProject1 = () => {
+    return (
+        <div>
+            <img src={require(`../images/logo192.png`)} alt="react logo" />
+            <h1>Fun Facts about React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100k stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </div>
+    )
+}
+
+export default ScrimbaProject1;
