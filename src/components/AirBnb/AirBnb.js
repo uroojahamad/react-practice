1217dd60eba2b8bcb4ba229bf416744f9e500131
Urooj Ahmad <uroojahamad786@gmail.com>
2023-01-29 15:17:47 +0530

    adding Airbnb,js

---
+++ src/components/AirBnb/AirBnb.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../../css/airbnb.css'
+import Card from './Card';
+import Header from './Header'
+import ImageGridText from './ImageGridText';
+
+const AirBnb = () => {
+    return (
+        <>
+            {/* <Header />
+            <ImageGridText /> */}
+            <Card />
+            <Card />
+            <Card />
+        </>
+    )
+}
+
+export default AirBnb;
