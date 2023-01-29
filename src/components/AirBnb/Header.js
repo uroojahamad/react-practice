f4c0fd47d5bb65b9eb844b9b74a8da2fa1655af2
Urooj Ahmad <uroojahamad786@gmail.com>
2023-01-29 15:19:28 +0530

    adding header component for Airbnb

---
+++ src/components/AirBnb/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='airbnb-header'>
+            <img src={require("../../images/airbnb-logo.png")} alt="airbnb logo" />
+        </header>
+    )
+}
+
+export default Header;
