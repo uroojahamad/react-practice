e9b263b7e6c72cab925a1ea4599bbab4ad50f895
Urooj Ahmad <uroojahamad786@gmail.com>
2023-02-07 22:22:16 +0530

    added textarea, radiobutton and checkbox

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,7 +7,9 @@ const Form = () => {
             lastName: "",
             email: "",
             comments: "",
-            isFriendly: true
+            isFriendly: true,
+            employement: ""
+
         }
     )
 
@@ -47,6 +49,59 @@ const Form = () => {
                     name="email"
                     value={formData.email}
                 />
+
+                {/* textarea */}
+                <textarea
+                    value={formData.comments}
+                    placeholder="Comments"
+                    onChange={handleChange}
+                    name="comments"
+                />
+
+                {/* checkbox */}
+                <input
+                    type="checkbox"
+                    id="isFriendly"
+                    checked={formData.isFriendly}
+                    onChange={handleChange}
+                    name="isFriendly"
+                />
+                <label htmlFor="isFriendly">Are you friendly?</label>
+                <br />
+
+                {/* radiobutton */}
+                <input
+                    type="radio"
+                    id="unemployed"
+                    name="employment"
+                    value="unemployed"
+                    onChange={handleChange}
+                    checked={formData.employment === "unemployed"}
+                />
+                <label htmlFor="unemployed">Unemployed</label>
+                <br />
+
+                <input
+                    type="radio"
+                    id="part-time"
+                    name="employment"
+                    value="part-time"
+                    onChange={handleChange}
+                    checked={formData.employment === "part-time"}
+                />
+                <label htmlFor="part-time">Part-time</label>
+                <br />
+
+                <input
+                    type="radio"
+                    id="full-time"
+                    name="employment"
+                    value="full-time"
+                    onChange={handleChange}
+                    checked={formData.employment === "full-time"}
+                />
+                <label htmlFor="full-time">Full-time</label>
+                <br />
             </form>
         </div>
     )
