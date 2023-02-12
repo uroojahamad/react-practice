123d71634f7883a5197fd4c1707edb70e8c379cb
Urooj Ahmad <uroojahamad786@gmail.com>
2023-02-12 18:20:02 +0530

    add dark mode toggle button

--- src/components/ScrimbaProject1.js
+++ src/components/ScrimbaProject1.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React,{useState} from 'react';
 import '../css/scrimbaproject1.css'
 
 const ScrimbaProject1 = () => {
+
+    const [darkMode, setDarkMode] = useState(true);
+
+    const toggleDarkMode = () =>{
+        setDarkMode(prevMode => !prevMode);
+    }
+    console.log(darkMode);
+    
     return (
-        <div className='scrimba-container'>
+    <div className={`scrimba-container ${darkMode ? 'dark' : ''}`}>
             <nav>
                 <img src={require(`../images/logo192.png`)} alt="react logo" />
                 <h3>ReactFacts</h3>
-                <h4>React Course - Project 1</h4>
+                <div className="toggler">
+                    <p className="toggler--light">Light</p>
+                    <div className="toggler--slider" onClick={toggleDarkMode}>
+                        <div className="toggler--slider--circle"></div>
+                    </div>
+                    <p className="toggler--dark">Dark</p>
+                </div>
             </nav>
             <main>
                 <h1 className='main--title'>Fun facts about React</h1>
